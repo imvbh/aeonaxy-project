e052757ec85dd3636c92bc84393b6935e2156b1a
Vaibhav <86156611+imvbh@users.noreply.github.com>
2024-04-08 00:12:39 +0530

    Routing

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import SignupForm from "./Components/SignupForm/SignupForm";
 import ImageLocation from "./Components/ImageLocation/ImageLocation";
 import Verification from "./Components/Verification/Verification"
 import SelectionPage from "./Components/SelectionPage/SelectionPage";
+import Notfound from "./Components/NotFound/Notfound";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/signup" element={<SignupForm/>}></Route>
+        <Route path="/" element={<SignupForm/>}></Route>
         <Route path="/create" element={<ImageLocation/>} />
         <Route path="/select" element={<SelectionPage/>} />
         <Route path="/verify" element={<Verification />} />
+        <Route path="*" element={<Notfound/>}/>
       </Routes>
     </BrowserRouter>
   );
